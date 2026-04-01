d88042a8ed8440b131cc5d5cbf5e253d293462e8
sebaslazar <sebastian.cacante@utp.edu.co>
2026-04-01 11:58:14 -0500

    Delet unnecessary files

--- Docker/run_workers.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [int]$N = 1
-)
-
-for ($i = 1; $i -le $N; $i++) {
-    docker run -d nn-worker python worker.py `
-        --server-host host.docker.internal `
-        --server-port 9999
-}
